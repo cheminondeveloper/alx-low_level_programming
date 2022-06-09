449f690b94a52f72803e31e4a3cb1e0f980c743c
cheminondeveloper <cheminon22@gmail.com>
2022-06-08 20:53:39 -0700

    task 5

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -11,7 +11,7 @@ int main(void)
 
 	r = print_sign(98);
 	_putchar(',');
-        _putchar(' ');
+	_putchar(' ');
 	_putchar(r + '0');
 	_putchar('\n');
 	r = print_sign(0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,5 @@ int _isalpha(int c);
 int _abs(int);
 int print_last_digit(int);
 void print_to_98(int n);
+void jack_bauer(void);
+int print_sign(int n);
